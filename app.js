1d744f4da2a4ebe01d31f82f65b033e72d5a295b
bill <williamandrewbain@gmail.com>
2018-09-17 10:14:29 -0600

    renamed variables and classes

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
 // Builds the canvas
 let container = document.querySelector('.container')
-
 for (var i = 0; i < 432; i++) {
-  let pixelSquare = document.createElement('div')
-  pixelSquare.setAttribute('class', 'pixSquare')
-  container.appendChild(pixelSquare)
+  let canvasSquare = document.createElement('div')
+  canvasSquare.setAttribute('class', 'canvasSquare')
+  container.appendChild(canvasSquare)
 }
 
 //Colors the canvas
 var chosenColor = ''
-
-container.addEventListener("click", function(event) {
+container.addEventListener("click", function (event) {
   event.target.style.backgroundColor = chosenColor
 })
 
 //Selects color
 let colors = document.querySelector('.colors')
-
-function handleColor (event) {
+function handleColor(event) {
   chosenColor = event.target.classList[1]
   currentColor.style.backgroundColor = chosenColor
 }
-
 colors.addEventListener("click", handleColor)
-
 
 //Handles the eraser
 let eraser = document.querySelector('.eraser')
-
-function erase () {
-    currentColor.style.backgroundColor = 'white'
-    chosenColor = 'white'
+function erase() {
+  currentColor.style.backgroundColor = 'white'
+  chosenColor = 'white'
 }
-
 eraser.addEventListener('click', erase)
